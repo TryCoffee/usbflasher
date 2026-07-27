64c633817a5276d0ef677683b55277283450e054
TryCoffee <108859537+TryCoffee@users.noreply.github.com>
2026-07-27 13:43:25 +0200

    Refactor message box functions in popups.py
    Refactor message box functions to use QWidget as parent and improve functionality.

--- popups.py
+++ popups.py
@@ -1,46 +1,34 @@
-from PyQt6.QtWidgets import QInputDialog, QLineEdit, QMessageBox
+"""Small wrappers around the Qt message boxes used by the main window."""
+
+from __future__ import annotations
+
+from PyQt6.QtWidgets import QMessageBox, QWidget
 
 
-def ask_password(self):
-    password, ok = QInputDialog.getText(self, "Password", "Enter your sudo password:",
-                                        QLineEdit.EchoMode.Password)
-    if ok and password:
-        return password
-    return None
+def ask_confirmation(parent: QWidget | None, title: str, message: str,
+                     details: str = "") -> bool:
+    box = QMessageBox(parent)
+    box.setWindowTitle(title)
+    box.setText(message)
+    box.setIcon(QMessageBox.Icon.Warning)
+    if details:
+        box.setInformativeText(details)
+    box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+    box.setDefaultButton(QMessageBox.StandardButton.No)
+    return box.exec() == QMessageBox.StandardButton.Yes
 
 
-def ask_confirmation(title, message):
-    msg = QMessageBox()
-    msg.setWindowTitle(title)
-    msg.setText(message)
-    msg.setIcon(QMessageBox.Icon.Question)
-    msg.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+def show_info(parent: QWidget | None, title: str, message: str) -> None:
+    box = QMessageBox(parent)
+    box.setWindowTitle(title)
+    box.setText(message)
+    box.setIcon(QMessageBox.Icon.Information)
+    box.exec()
 
-    result = msg.exec()
-    return result == QMessageBox.StandardButton.Yes
 
-def ask_native_or_flatpak(option1, option2, title, message):
-    msg = QMessageBox()
-    msg.setWindowTitle(title)
-    msg.setText(message)
-    msg.setIcon(QMessageBox.Icon.Question)
-
-    button1 = msg.addButton(option1, QMessageBox.ButtonRole.AcceptRole)
-    button2 = msg.addButton(option2, QMessageBox.ButtonRole.AcceptRole)
-    msg.addButton("Cancel", QMessageBox.ButtonRole.RejectRole)
-    msg.exec()
-
-    if msg.clickedButton() == button1:
-        return option1
-    elif msg.clickedButton() == button2:
-        return option2
-    else:
-        return None  #for cancel or any other case
-
-def show_info(title, message):
-    msg = QMessageBox()
-    msg.setWindowTitle(title)
-    msg.setText(message)
-    msg.setIcon(QMessageBox.Icon.Information)
-    msg.setStandardButtons(QMessageBox.StandardButton.Ok)
-    msg.exec()
+def show_error(parent: QWidget | None, title: str, message: str) -> None:
+    box = QMessageBox(parent)
+    box.setWindowTitle(title)
+    box.setText(message)
+    box.setIcon(QMessageBox.Icon.Critical)
+    box.exec()
